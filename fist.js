1f3a4282f0ff0b87d9384f8ab966f48377506191
bhushansable <bhushansable9876@gmail.com>
2025-12-05 11:38:44 +0530

    Add shooting star animations and update mode toggle functionality

--- fist.js
+++ fist.js
@@ -5,11 +5,21 @@ mode.addEventListener("click",()=>{
         curMode="light"
         document.querySelector("#main").style.backgroundColor ="white";
         document.querySelector("#main").style.Color ="black";
+        document.querySelector("#star1").style.backgroundColor ="#00E5FF"
+        document.querySelector("#star2").style.backgroundColor ="#00E5FF"
+        document.querySelector("#star3").style.backgroundColor ="#00E5FF"
+        document.querySelector("#star4").style.backgroundColor ="#00E5FF"
+
+
 
     }else{
         curMode="dark"
         document.querySelector("#main").style.backgroundColor ="#0f0f0f";
            document.querySelector("#main").style.Color ="white";
+         document.querySelector("#star1").style.backgroundColor ="white"
+        document.querySelector("#star2").style.backgroundColor ="white"
+        document.querySelector("#star3").style.backgroundColor ="white"
+        document.querySelector("#star4").style.backgroundColor ="white"
     }
     console.log(curMode);
 })
