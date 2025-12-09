62233981a2b7cac087ec295c271defc0f3e540c7
bhushansable <bhushansable9876@gmail.com>
2025-12-09 13:52:29 +0530

    Refactor mode toggle functionality into a dedicated function for improved readability

--- fist.js
+++ fist.js
@@ -1,6 +1,11 @@
-let mode = document.querySelector("#Mode");
-let curMode ="dark"
-mode.addEventListener("click",()=>{
+
+let mode_fun =()=>{
+
+    let mode = document.querySelector("#Mode");
+    let curMode ="dark"
+
+
+    mode.addEventListener("click",()=>{
     if(curMode==="dark"){
         curMode="light"
         document.querySelector("#main").style.backgroundColor ="white";
@@ -15,11 +20,15 @@ mode.addEventListener("click",()=>{
     }else{
         curMode="dark"
         document.querySelector("#main").style.backgroundColor ="#0f0f0f";
-           document.querySelector("#main").style.Color ="white";
-         document.querySelector("#star1").style.backgroundColor ="white"
+        document.querySelector("#main").style.Color ="white";
+        document.querySelector("#star1").style.backgroundColor ="white"
         document.querySelector("#star2").style.backgroundColor ="white"
         document.querySelector("#star3").style.backgroundColor ="white"
         document.querySelector("#star4").style.backgroundColor ="white"
     }
     console.log(curMode);
 })
+
+}
+
+mode_fun()
